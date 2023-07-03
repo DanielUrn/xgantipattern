98a9409a1196a0a380bf8da2bd4d82493f7a9a31
Daniel Urdaneta <danielurn@hotmail.com>
2023-07-02 23:12:37 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import jeff from './img/jeff2.png'
+import ansel from './img/ansel2.png'
+import team from './img/team.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='container'>
+        <div className='d-flex flex-sm-column flex-lg-row'>
+          <div>
+            <img className='w-50' src={jeff}></img>
+            <img className='w-50' src={ansel}></img>
+            <img className='w-75' src={team}></img>
+          </div>
+          <div className="flex-grow-1">
+            <h1> Sit commodo elit dolore sint proident sunt minim pariatur velit esse. Ex id ea occaecat eu incididunt eu ea eiusmod consequat. Ad aute elit cillum do dolore consequat sint. </h1>
+          </div>
+        </div>
+      </div>
+      <div className='footer'>GOL</div>
     </div>
   );
 }
