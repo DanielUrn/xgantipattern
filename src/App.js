ca37aeee743b382dc71f1e5b9d8fc9b7b82ba29c
Daniel Urdaneta <danielurn@hotmail.com>
2023-07-03 22:53:19 -0400

    layout built

--- src/App.js
+++ src/App.js
@@ -4,23 +4,70 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import jeff from './img/jeff2.png'
 import ansel from './img/ansel2.png'
 import team from './img/team.png'
+import { Form, Button } from 'react-bootstrap';
+import React, { useState } from 'react';
+
 
 function App() {
+
+  const [email, setEmail] = useState('');
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Add your email handling logic here
+    console.log('Submitted email:', email);
+    // Reset the form
+    setEmail('');
+  };
+
   return (
     <div>
       <div className='container'>
-        <div className='d-flex flex-sm-column flex-lg-row'>
-          <div>
-            <img className='w-50' src={jeff}></img>
-            <img className='w-50' src={ansel}></img>
-            <img className='w-75' src={team}></img>
-          </div>
+        <div className='d-flex flex-md-row flex-column justify-content-center'>
+          {/* <div className='d-flex flex-sm-row flex-lg-column flex-grow-1'>
+            <img className='w-100' src={jeff} alt='Jeff pfp'></img>
+            <img className='w-100' src={ansel} alt='Ansel pfp'></img>
+          </div> */}
+          <img className='w-100 m-auto' src={team} alt='Team pfps'></img>
           <div className="flex-grow-1">
-            <h1> Sit commodo elit dolore sint proident sunt minim pariatur velit esse. Ex id ea occaecat eu incididunt eu ea eiusmod consequat. Ad aute elit cillum do dolore consequat sint. </h1>
+            <h2 className='text-muted'>We are <span className='fw-bold'>XGlobal!</span></h2>
+            <h1>Empowering Web 3 Growth</h1>
+            <p className='text-body-secondary'>We inspire innovation. As a flexible and adaptable consulting business, we partner with experts to provide groundbreaking services for your success.
+              <h2>Our offerings include:</h2>
+              <ul>
+                <li>Tournament management, and robust growth consultancy. </li>
+                <li>Social media mastery</li>
+                <li>Graphic design wizardry</li>
+                <li>Cutting-edge web development</li>
+                <li>Extensive web 3 project research</li>
+                <li>Robust Growth Consultancy</li>
+              </ul>
+              Embrace the new wave, drive exponential growth.</p>
+            <Form className='m-3'>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Text className="text-muted">
+                  We'll never share your email with anyone else.
+                </Form.Text>
+              </Form.Group>
+              <Button variant="dark" type="submit">
+                Submit
+              </Button>
+            </Form>
           </div>
         </div>
       </div>
-      <div className='footer'>GOL</div>
+      <div className='footer'>
+        <div className=''>
+          
+        </div>
+        <div></div>
+      </div>
     </div>
   );
 }
