f91b57198da9a9353147fb26200b678e5f199b25
Daniel Urdaneta <danielurn@hotmail.com>
2023-07-06 14:52:49 -0400

    home and navbar components refactored

--- src/components/home.js
+++ src/components/home.js
@@ -1,50 +1,56 @@
-import React from 'react'
-import { Form, Button } from 'react-bootstrap';
-import jeff from '../img/jeff2.png'
-import ansel from '../img/ansel2.png'
-import team from '../img/team.png'
+import React from "react";
+import { Form, Button, Container, Row, Col } from "react-bootstrap";
+import jeff from "../img/jeff2.png";
+import ansel from "../img/ansel2.png";
+import team from "../img/jeff2.png";
 
 const Home = () => {
   return (
     <div>
-      <div className='container'>
-        <div className='d-flex flex-md-row flex-column justify-content-center'>
-          {/* <div className='d-flex flex-sm-row flex-lg-column flex-grow-1'>
-            <img className='w-100' src={jeff} alt='Jeff pfp'></img>
-            <img className='w-100' src={ansel} alt='Ansel pfp'></img>
-          </div> */}
-          <img className='w-100 m-auto' src={team} alt='Team pfps'></img>
-          <div className="flex-grow-1">
-            <h2 className='text-muted'>We are <span className='fw-bold'>XGlobal!</span></h2>
-            <h1>Empowering Web 3 Growth</h1>
-            <p className='text-body-secondary'>We inspire innovation. As a flexible and adaptable consulting business, we partner with experts to provide groundbreaking services for your success.</p>
-            <h2>Our offerings include:</h2>
-            <ul>
-              <li>Tournament management, and robust growth consultancy. </li>
-              <li>Social media mastery</li>
-              <li>Graphic design wizardry</li>
-              <li>Cutting-edge web development</li>
-              <li>Extensive web 3 project research</li>
-              <li>Robust Growth Consultancy</li>
-            </ul>
-            <h3>Embrace the new wave, drive exponential growth.</h3>
-            <Form className='m-3'>
-              <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-                <Form.Text className="text-muted">
-                  We'll never share your email with anyone else.
-                </Form.Text>
-              </Form.Group>
-              <Button variant="dark" type="submit">
-                Submit
-              </Button>
-            </Form>
-          </div>
-        </div>
-      </div>
+      <Container className="pt-3">
+        <Row>
+          <Col lg>
+            <img className="w-75 m-auto" src={team} alt="Team pfps"></img>
+          </Col>
+          <Col lg>
+            <div className="p-2">
+              <h2 className="text-muted">
+                We are <span className="fw-bold">XGlobal!</span>
+              </h2>
+              <h1>Empowering Web 3 Growth</h1>
+              <p className="text-body-secondary">
+                We inspire innovation. As a flexible and adaptable consulting
+                business, we partner with experts to provide groundbreaking
+                services for your success.
+              </p>
+              <h2>Our offerings include:</h2>
+              <ul>
+                <li>Tournament management, and robust growth consultancy. </li>
+                <li>Social media mastery</li>
+                <li>Graphic design wizardry</li>
+                <li>Cutting-edge web development</li>
+                <li>Extensive web 3 project research</li>
+                <li>Robust Growth Consultancy</li>
+              </ul>
+              <h3>Embrace the new wave, drive exponential growth.</h3>
+              <Form className="m-3">
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Label>Email address</Form.Label>
+                  <Form.Control type="email" placeholder="Enter email" />
+                  <Form.Text className="text-muted">
+                    We'll never share your email with anyone else.
+                  </Form.Text>
+                </Form.Group>
+                <Button variant="dark" type="submit">
+                  Submit
+                </Button>
+              </Form>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,18 +1,32 @@
-import React from 'react'
-import { Container, Navbar, Nav, NavDropdown } from 'react-bootstrap'
-import logo from '../img/navbrand.png'
+import React from "react";
+import { Container, Navbar, Nav, NavDropdown } from "react-bootstrap";
+import logo from "../img/navbrand.png";
 const Navigation = () => {
   return (
-    <Navbar expand="lg" className="bg-primary" sticky='top'>
+    <Navbar expand="lg" className="bg-primary" sticky="top">
       <Container>
-      <Navbar.Brand href="#home" className='d-lg-none'> <img src={logo} alt='logo' width={'150px'} height={'50px'}></img> </Navbar.Brand>
+        <Navbar.Brand href="#home" className="d-lg-none">
+          <img 
+            src={logo} 
+            alt="logo" 
+            width={"150px"} 
+            height={"50px"}>  
+          </img>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav" className=''>
-          <Nav className='align-items-center justify-content-between flex-grow-1'>
+        <Navbar.Collapse id="basic-navbar-nav" className="">
+          <Nav className="align-items-center justify-content-between flex-grow-1">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#services">Services</Nav.Link>
 
-        <Navbar.Brand href="#home" className='d-none d-lg-inline-block'> <img src={logo} alt='logo' width={'150px'} height={'50px'}></img> </Navbar.Brand>
+            <Navbar.Brand href="#home" className="d-none d-lg-inline-block">
+              <img
+                src={logo}
+                alt="logo"
+                width={"150px"}
+                height={"50px"}
+              ></img>
+            </Navbar.Brand>
 
             <Nav.Link href="#link">Link</Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
@@ -30,7 +44,7 @@ const Navigation = () => {
         </Navbar.Collapse>
       </Container>
     </Navbar>
-  )
-}
+  );
+};
 
-export default Navigation
+export default Navigation;
