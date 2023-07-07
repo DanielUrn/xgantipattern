9067ec8ca93cc538cf3821e6dd20485c257315e3
Daniel Urdaneta <danielurn@hotmail.com>
2023-07-07 11:29:39 -0400

    button on home fixed

--- src/components/home.js
+++ src/components/home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Form, Button, Container, Row, Col } from "react-bootstrap";
+import { Form, Button, Container, Row, Col, InputGroup } from "react-bootstrap";
 import jeff from "../img/jeff2.png";
 import ansel from "../img/ansel2.png";
 import team from "../img/jeff2.png";
@@ -10,41 +10,40 @@ const Home = () => {
       <Container className="pt-5">
         <Row>
           <Col className="d-flex align-content-center" lg >
-            <img className="m-auto h-75" src={team} alt="Team pfps"></img>
+            <img className="m-auto mw-100 h-100" src={team} alt="Team pfps"></img>
           </Col>
-          <Col lg>
+          <Col lg className="text-center text-lg-start">
             <div className="pt-2">
               <h2 className="text-muted">
-                We are <span className="fw-bold">XGlobal!</span>
+                Hey there, I'm Jeff from XGlobal Consulting!
               </h2>
-              <h1>Empowering Web 3 Growth</h1>
+              <h1><span>Unlock</span> the Web3 World with Our <span>Expert Guidance</span></h1>
               <p className="text-body-secondary">
-                We inspire innovation. As a flexible and adaptable consulting
-                business, we partner with experts to provide groundbreaking
-                services for your success.
+                We've <span>experienced</span> the up's, survived the downs. <br></br>
+                And we want to help you do the same
               </p>
-              <h2>Our offerings include:</h2>
               <ul>
-                <li>Tournament management, and robust growth consultancy. </li>
-                <li>Social media mastery</li>
-                <li>Graphic design wizardry</li>
-                <li>Cutting-edge web development</li>
-                <li>Extensive web 3 project research</li>
-                <li>Robust Growth Consultancy</li>
+                <li> <span>Dependable</span> consulting partner to pioneers </li>
+                <li>Book a <span>no-bullsh*t</span> meeting with someone who speaks your language. </li>
+                <li>Tired of putting out fires? <span>That's where we come in</span></li>
               </ul>
-              <h3>Embrace the new wave, drive exponential growth.</h3>
-              <Form className="m-3">
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                  <Form.Label>Email address</Form.Label>
-                  <Form.Control type="email" placeholder="Enter email" />
-                  <Form.Text className="text-muted">
-                    We'll never share your email with anyone else.
-                  </Form.Text>
-                </Form.Group>
-                <Button variant="dark" type="submit">
-                  Submit
-                </Button>
-              </Form>
+              <h3>Disappointed but undeterred? <br></br> <span>Show us your determination</span> </h3>
+              <Row className="justify-content-center justify-content-lg-start pt-3">
+                <Col sm='7' md='8'>
+                  <InputGroup className="mb-3">
+                    <Form.Control
+                      placeholder="Enter your Telegram username"
+                      aria-label="Recipient's username"
+                      aria-describedby="basic-addon2"
+                      className="rounded-0"
+                    />
+                    <Button className="rounded-0 bg-dark" variant="outline-secondary" id="button-addon2">
+                      LET'S TALK
+                    </Button>
+                  </InputGroup>
+                </Col>
+              </Row>
+
             </div>
           </Col>
         </Row>
