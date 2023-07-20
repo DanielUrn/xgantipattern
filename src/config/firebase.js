3f8d03911530429a123e829bb1532eb8c8aa4513
Daniel Urdaneta <danielurn@hotmail.com>
2023-07-20 18:49:21 -0400

    firebase cred secure

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,8 +1,12 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import firebaseJson from '../xgantipatternFirebase.json'
+const fs = require('fs');
+const path = require('path');
 
-const app = initializeApp(firebaseJson);
+const filePath = path.join(__dirname, 'firebase-cred.json');
+const jsonDataString = fs.readFileSync(filePath, 'utf-8');
+const jsonData = JSON.parse(jsonDataString);
+const app = initializeApp(jsonData);
 const db = getFirestore(app);
 
 export default db;
