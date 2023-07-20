8241168fb473f52c059331ef365044410f27cbad
Daniel Urdaneta <danielurn@hotmail.com>
2023-07-20 18:59:44 -0400

    blogs working?

--- src/components/blog.js
+++ src/components/blog.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 
 const Blog = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://https://consultingxglobal.wordpress.com/wp-json/wp/v2/posts')
+      .then((response) => setPosts(response.data))
+      .catch((error) => console.error(error));
+  }, []);
+
   return (
     <div>
-      <div>Filters</div>
-      
+      <div>Blogs</div>
+      <ul>
+        {posts.map((post)=>{
+          <li key={post.id}>{post.title.rendered}</li>
+        })}
+      </ul>
     </div>
   )
 }
